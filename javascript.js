b6e670dd15a0b2d5c2216064f3724319c436e41a
Muneeb ur Rehman <muneeb.ur.rehman60@gmail.com>
2023-05-31 15:50:33 +0500

    Update javascript.js
    CartState from localStorage.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,3 @@
-console.log('abc+32')
-console.log('print3', self)
+let cartStateObj = JSON.parse(localStorage.getItem("cartState"));
+console.log('cartStare',cartStateObj);
+console.log('print3', self);
